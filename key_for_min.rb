e024f40d67a94ba2b2f85f62beddd97d48fcaf53
Brennan Heisler <learn@heislercreative.com>
2018-01-28 00:12:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
-      
+      lowest_key = key
+      lowest_value = value
   end
   lowest_key
 end
